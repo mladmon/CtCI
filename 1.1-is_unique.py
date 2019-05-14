595aba72d6fae1b650850c509504f0c2b0ff08ca
Mario Admon <mladmon589@gmail.com>
2019-05-13 22:05:58 -0400

    Implemented optimal restricted version... sort then compare two consecutive chars

--- 1.1-is_unique.py
+++ 1.1-is_unique.py
@@ -1,3 +1,4 @@
+# no restrictions - O(n) runitme, O(n) space (all from creating the set!)
 def is_unique(string):
 	#return len(set(string)) == len(string)
 	return len(set(string)) is len(string)
@@ -10,7 +11,7 @@ print('whatboudis?', is_unique('whatboudis?'))
 print('whataboudis?', is_unique('whataboudis?'))
 print('h', is_unique('h'))
 print('', is_unique(''))
-
+print()
 
 # restricted no data structures allowed - O(n^2), O(1)
 def is_unique2(string):
@@ -28,4 +29,22 @@ print('whatboudis?', is_unique2('whatboudis?'))
 print('whataboudis?', is_unique2('whataboudis?'))
 print('h', is_unique2('h'))
 print('', is_unique2(''))
+print()
+
+# restricted, better runtime - O(n*log n), O(1)
+def is_unique3(string):
+	s = sorted(string)
+	for i in range(1, len(string)):
+		if s[i-1] is s[i]:
+			return False
+	return True
+
+# Let's test it!
+print("hello, world!", is_unique3("hello, world!"))
+print('mario', is_unique3("mario"))
+print('Mm', is_unique3("Mm"))
+print('whatboudis?', is_unique3('whatboudis?'))
+print('whataboudis?', is_unique3('whataboudis?'))
+print('h', is_unique3('h'))
+print('', is_unique3(''))
 
