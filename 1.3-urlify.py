6772ba6ff86c618d0becd166294adc90a4eaf85b
Mario Admon <mladmon589@gmail.com>
2019-05-15 18:00:52 -0400

    Oops! Made a noob Python mistake... strings are immutable so can't modify input string

--- 1.3-urlify.py
+++ 1.3-urlify.py
@@ -1,10 +1,5 @@
-# python str methods make this problem very easy... yay python!
-# this version modifies the input string
+# python str methods make this problem very easy
 def urlify(string):
-	string = string.rstrip().replace(' ', '%20')
-
-# this version returns a URLified copy
-def urlify2(string):
 	return string.rstrip().replace(' ', '%20')
 
 # Let's test it!
@@ -12,7 +7,7 @@ s1 = "mario admon"
 s2 = "mario admon   "
 s3 = "mario  admon  "
 s4 = "This sentence has several spaces   "
-print(repr(s1), urlify2(s1))
-print(repr(s2), urlify2(s2))
-print(repr(s3), urlify2(s3))
-print(repr(s4), urlify2(s4))
+print(repr(s1), urlify(s1))
+print(repr(s2), urlify(s2))
+print(repr(s3), urlify(s3))
+print(repr(s4), urlify(s4))
