ff93a26c4a86b8f05f542681ef2f8566d8e4b7da
Mario Admon <mladmon589@gmail.com>
2019-05-16 21:56:33 -0400

    Implemented 1.4 palindrome permutation - O(n), O(1) bit array solution

--- 1.4-palindrome_perm.py
+++ 1.4-palindrome_perm.py
@@ -8,9 +8,25 @@ def palindrome_perm(string):
 			num_odd += 1
 	return num_odd < 2
 
+def palindrome_perm2(string):
+    freq = [0] * 256 
+    for c in string.lower().replace(' ', ''):
+        freq[ord(c)] += 1
+    num_odd = 0 
+    for f in freq:
+        if f % 2 is 1:
+            num_odd += 1
+    return num_odd < 2 
+
 # Let's test it!
 print('Tact Coa', palindrome_perm('Tact Coa'))
 print('mario is the thorns man', palindrome_perm('mario is the thorns man'))
 print(repr(''), palindrome_perm(''))
 print('tttaaaabbdddddd', palindrome_perm('tttaaaabbdddddd'))
 print('tttaaaabbddddd', palindrome_perm('tttaaaabbddddd'))
+
+print('Tact Coa', palindrome_perm2('Tact Coa'))
+print('mario is the thorns man', palindrome_perm2('mario is the thorns man'))
+print(repr(''), palindrome_perm2(''))
+print('tttaaaabbdddddd', palindrome_perm2('tttaaaabbdddddd'))
+print('tttaaaabbddddd', palindrome_perm2('tttaaaabbddddd'))
