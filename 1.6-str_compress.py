e4a692b6a3a8515aad5dc844c48fc72bd9cbef5b
Mario Admon <mladmon589@gmail.com>
2019-05-20 14:05:54 -0400

    1.6) String Compression - implemented the slightly better solution from the book

--- 1.6-str_compress.py
+++ 1.6-str_compress.py
@@ -14,6 +14,20 @@ def str_compress(string):
 	else:
 		return string
 
+def str_compress2(string):
+	if len(string) < 3:
+		return string
+	comp, count = [], 0
+	for i in range(len(string)):
+		count += 1
+		if i + 1 is len(string) or string[i+1] is not string[i]:
+			comp.extend((string[i], str(count)))
+			count = 0
+	if len(comp) < len(string):
+		return ''.join(comp)
+	else:
+		return string
+
 # Let's test it!
 print('aabcccccaaa', str_compress('aabcccccaaa'))
 print(repr(''), str_compress(''))
@@ -27,3 +41,17 @@ print('aaabb', str_compress('aaabb'))
 print('Ooooooh!!', str_compress('Ooooooh!!'))
 print('oooOoOOO', str_compress('oooOoOOO'))
 print('oooOOoOOO', str_compress('oooOOoOOO'))
+
+print()
+print('aabcccccaaa', str_compress2('aabcccccaaa'))
+print(repr(''), str_compress2(''))
+print('a', str_compress2('a'))	
+print('aa', str_compress2('aa'))
+print('ab', str_compress2('ab'))
+print('aaa', str_compress2('aaa'))
+print('aabb', str_compress2('aabb'))
+print('aaab', str_compress2('aaab'))
+print('aaabb', str_compress2('aaabb'))
+print('Ooooooh!!', str_compress2('Ooooooh!!'))
+print('oooOoOOO', str_compress2('oooOoOOO'))
+print('oooOOoOOO', str_compress2('oooOOoOOO'))
