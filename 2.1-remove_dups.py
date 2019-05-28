742750fdd856d0881acb9c9e888a985b541365ea
Mario Admon <mladmon589@gmail.com>
2019-05-28 17:53:43 -0400

    the first properly-implemented module (i.e., one you can import OR run as a script) that also imports names from another module

---
+++ 2.1-remove_dups.py
@@ -0,0 +1,20 @@
+from linked_list import *
+
+def remove_dups(head):
+	pass
+
+def remove_dups2(head):
+	pass
+
+# Let's test it!
+if __name__ == '__main__':
+	head = Node(2)
+	head.append(3)
+	head.append(7)
+	head.append(2)
+	head.append(3)
+	head.append(5)
+	head.append(1)
+	head.append(2)
+
+	print_l(head)
