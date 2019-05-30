a8ed58427534d2565d21d21004011f66933e043d
Mario Admon <mladmon589@gmail.com>
2019-05-30 19:43:38 -0400

    Implemented 2.4) Partition - O(n), O(1) optimal solution... holy ef

--- 2.4-partition.py
+++ 2.4-partition.py
@@ -1,32 +1,36 @@
 from linked_list import *
 
 def partition(head, x):
-	part_node, n, new_head, sec_to_last = None, head, head, None
+	last_lt, last_gt_eq, n, new_head = None, None, head, head
 	while n:
-		print(n.data)
 		if n.data < x:
 			# move node n
-			if part_node:
+			if last_lt:
 				temp = Node(n.data)
-				temp.next = part_node.next
-				part_node.next = temp
-				part_node = temp
+				temp.next = last_lt.next
+				last_lt.next = temp
+				last_lt = temp
 			else:
-				part_node = Node(n.data)
-				part_node.next = head
-				new_head = part_node
+				last_lt = Node(n.data)
+				last_lt.next = head
+				new_head = last_lt
 			# remove old node n
-			if n.next: # n is not the last node
-				if n.next.next is None:
-					sec_to_last = n
+			if n.next:
 				n.data = n.next.data
 				n.next = n.next.next
-			else: # n is the last node
-				if sec_to_last:
-					sec_to_last.next = None	
+			else:
+				if last_gt_eq:
+					last_gt_eq.next = None
+				else:
+					last_lt.next = None
+				break
 		else:
+			last_gt_eq = n
 			n = n.next
-	return new_head
+	if new_head:
+		return new_head
+	else:
+		return head
 
 # Let's test it!
 l1 = Node(3)
@@ -37,6 +41,14 @@ l1.append(10)
 l1.append(2)
 l1.append(1)
 
+l2 = Node(3)
+l2.append(1)
+
 print_l(l1)
 head = partition(l1, 5)
 print_l(head)
+
+print_l(l2)
+head = partition(l2, 5)
+print_l(head)
+print_l(partition(head, 2))
