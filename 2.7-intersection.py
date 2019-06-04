a9faa172b313115c09dcc3a3121bb8afdd8a1be3
Mario Admon <mladmon589@gmail.com>
2019-06-04 16:01:40 -0400

    Implemented 2.7) Intersection - O(n+m), O(1) optimal solution (I was wrong, you can solve it w/ constant space)

--- 2.7-intersection.py
+++ 2.7-intersection.py
@@ -12,6 +12,32 @@ def intersection(l1, l2):
 		n2 = n2.next
 	return None
 
+def intersection2(l1, l2):
+	size1, size2, n1, n2 = 1, 1, l1, l2
+	if n1 is None or n2 is None:
+		return None
+	while n1.next:
+		size1 += 1
+		n1 = n1.next
+	while n2.next:
+		size2 += 1
+		n2 = n2.next
+	if n1 is not n2:
+		return None
+	else:
+		if size1 >= size2:
+			return find_intersect(l1, l2, size1-size2)
+		else:
+			return find_intersect(l2, l1, size2-size1)
+
+def find_intersect(l1, l2, k):
+	n1, n2 = l1, l2
+	for i in range(k):
+		n1 = n1.next
+	while n1 is not n2:
+		n1, n2 = n1.next, n2.next
+	return n1
+
 # Let's test it!
 l1 = Node(7)
 l1.append(3)
@@ -36,6 +62,6 @@ print('3:', l2.next.next)
 print('l3:', end=' ')
 print_l(l3)
 print('3:', l3.next.next)
-print('intersection(l1, l2):', intersection(l1, l2))
-print('intersection(l1, l3):', intersection(l1, l3))
-print('intersection(l2, l3):', intersection(l2, l3))
+print('intersection(l1, l2):', intersection2(l1, l2))
+print('intersection(l1, l3):', intersection2(l1, l3))
+print('intersection(l2, l3):', intersection2(l2, l3))
