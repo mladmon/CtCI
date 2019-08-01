baca7f8ef14f7e06a835ce67aa000d75896b1690
Mario Admon <mladmon589@gmail.com>
2019-08-01 16:53:56 -0700

    Moved check if num is -1 to beginning; added logic to properly update curSeq to 0 if next bit is a 0. Original solution still works but this seems safer

--- 05-bit_manipulation/5.3-flip_bit.cpp
+++ 05-bit_manipulation/5.3-flip_bit.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 int flipBit(int num) {
+	if (num == -1)
+		return sizeof(int)*8;
+
 	int curSeq = 0, nextSeq = 0, longestSeq = 0;
 	int runningCount = 0;
 	for (int i=0; i < sizeof(int)*8; ++i) {
@@ -13,14 +16,11 @@ int flipBit(int num) {
 			if (nextSeq + curSeq + 1 > longestSeq) {
 				longestSeq = nextSeq + curSeq + 1;
 			}
-			curSeq = nextSeq;
+			curSeq = (num & 2) ? nextSeq : 0;
 			runningCount = 0;
 		}
 		num >>= 1;
 	}
-
-	if (runningCount == 32)
-		return runningCount;
 
 	// could've ended on a 1, check last sequence
 	if (runningCount + curSeq + 1 > longestSeq)
@@ -36,7 +36,9 @@ int main() {
 				<< flipBit(0b0111111011011101111) << endl;
 	cout << "flipBit(0b111111011011101111): " 
 				<< flipBit(0b111111011011101111) << endl;
+	cout << "flipBit(0b1111110011011101111): " 
+				<< flipBit(0b1111110011011101111) << endl;
+	cout << "flipBit(0b10100001): " << flipBit(0b10100001) << endl;
 
-	
 	return 0;
 }
