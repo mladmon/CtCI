c0276b9231924d23b1b072fb7276266128cf2ac9
Mario Admon <mladmon589@gmail.com>
2019-12-16 14:03:35 -0800

    Cleaned the solution now that I know more about Python

--- 01-arrays_strings/1.5-one_away.py
+++ 01-arrays_strings/1.5-one_away.py
@@ -1,41 +1,50 @@
-def one_away(str1, str2):
-	size_diff = abs(len(str1)-len(str2))
+# O(n) runtime, O(1) space solution
+def is_one_away(str1, str2):
+	size_diff = abs(len(str1) - len(str2))
 	if size_diff > 1:
 		return False
-	elif size_diff is 1:
+	elif size_diff == 1:
 		if len(str1) < len(str2):
-			return diff_by_one(str1, str2)
+			return check_one_insert_away(str1, str2)
 		else:
-			return diff_by_one(str2, str1)
+			return check_one_insert_away(str2, str1)
 	else:
-		return same_size(str1, str2)
+		return check_one_replace_away(str1, str2)
 
-def diff_by_one(subs, string):
+
+def check_one_insert_away(smaller, larger):
+	# i is smaller's index, j is larger's index
 	j = 0
-	for i in range(len(subs)):
-		if j is len(string): # more than one char was different
-			return False
-		if subs[i] is string[j]:
+	made_edit = False
+	for i in range(len(smaller)):
+		if smaller[i] == larger[j]:
 			j += 1
-		elif subs[i] is string[j + 1]:
-			j += 2
-		else:
-			return False
-	return True
-
-def same_size(str1, str2):
-	one_diff = False
-	for i in range(len(str1)):
-		if str1[i] is not str2[i]:
-			if one_diff:
+		elif smaller[i] == larger[j + 1]:
+			if made_edit:
 				return False
 			else:
-				one_diff = True
+				made_edit = True
+				j += 2
+		else:
+			return False
+
 	return True
 
+
+def check_one_replace_away(str1, str2):
+	found_diff = False
+	for i in range(len(str1)):
+		if str1[i] != str2[i]:
+			if found_diff:
+				return False
+			else:
+				found_diff = True
+	return True
+
+
 # Let's test it!
-print('pale', 'ple', one_away('pale', 'ple'))
-print('pales', 'pale', one_away('pales', 'pale'))
-print('pale', 'bale', one_away('pale', 'bale'))
-print('pale', 'bake', one_away('pale', 'bake'))
-print('apxpl', 'pple', one_away('apxpl', 'pple')) #out of bounds
+print('pale,', 'ple ->', is_one_away('pale', 'ple'))
+print('pales,', 'pale ->', is_one_away('pales', 'pale'))
+print('pale,', 'bale ->', is_one_away('pale', 'bale'))
+print('pale,', 'bake ->', is_one_away('pale', 'bake'))
+print('apxpl,', 'pple ->', is_one_away('apxpl', 'pple'))
