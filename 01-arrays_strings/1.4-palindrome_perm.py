c163e83130cd150477a4e53a19cadbd67019bc9f
Mario Admon <mladmon589@gmail.com>
2019-12-16 12:57:28 -0800

    Cleaned the solution now that I know more about Python

--- 01-arrays_strings/1.4-palindrome_perm.py
+++ 01-arrays_strings/1.4-palindrome_perm.py
@@ -1,32 +1,39 @@
-def palindrome_perm(string):
-	d = {}
-	for c in string.lower().replace(' ', ''):
-		d[c] = d.get(c, 0) + 1
+# O(n) runtime, O(n) space solution
+def is_palindrome_perm(string):
+	char_freq = {}
+	# assumes case-insensitive and removes whitespace
+	for char in string.lower().replace(' ', ''):
+		char_freq[char] = char_freq.get(char, 0) + 1
+
 	num_odd = 0
-	for v in d.values():
-		if v % 2 is 1:
+	for freq in char_freq.values():
+		if freq % 2 == 1:
 			num_odd += 1
+
 	return num_odd < 2
 
-def palindrome_perm2(string):
-    freq = [0] * 256 
-    for c in string.lower().replace(' ', ''):
-        freq[ord(c)] += 1
+# O(n) runtime, O(1) space solution (could argue above is constant too)
+def is_palindrome_perm2(string):
+    char_freq = [0] * 256
+    for char in string.lower().replace(' ', ''):
+        char_freq[ord(char)] += 1
+
     num_odd = 0 
-    for f in freq:
-        if f % 2 is 1:
+    for freq in char_freq:
+        if freq % 2 == 1:
             num_odd += 1
+
     return num_odd < 2 
 
 # Let's test it!
-print('Tact Coa', palindrome_perm('Tact Coa'))
-print('mario is the thorns man', palindrome_perm('mario is the thorns man'))
-print(repr(''), palindrome_perm(''))
-print('tttaaaabbdddddd', palindrome_perm('tttaaaabbdddddd'))
-print('tttaaaabbddddd', palindrome_perm('tttaaaabbddddd'))
+print('Tact Coa', is_palindrome_perm('Tact Coa'))
+print('mario is the thorns man', is_palindrome_perm('mario is the thorns man'))
+print(repr(''), is_palindrome_perm(''))
+print('tttaaaabbdddddd', is_palindrome_perm('tttaaaabbdddddd'))
+print('tttaaaabbddddd', is_palindrome_perm('tttaaaabbddddd'))
 
-print('Tact Coa', palindrome_perm2('Tact Coa'))
-print('mario is the thorns man', palindrome_perm2('mario is the thorns man'))
-print(repr(''), palindrome_perm2(''))
-print('tttaaaabbdddddd', palindrome_perm2('tttaaaabbdddddd'))
-print('tttaaaabbddddd', palindrome_perm2('tttaaaabbddddd'))
+print('Tact Coa', is_palindrome_perm2('Tact Coa'))
+print('mario is the thorns man', is_palindrome_perm2('mario is the thorns man'))
+print(repr(''), is_palindrome_perm2(''))
+print('tttaaaabbdddddd', is_palindrome_perm2('tttaaaabbdddddd'))
+print('tttaaaabbddddd', is_palindrome_perm2('tttaaaabbddddd'))
