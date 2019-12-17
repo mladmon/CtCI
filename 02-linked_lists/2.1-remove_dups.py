82e617ecbfb1cb180c8e7affced78242b3c29a25
Mario Admon <mladmon589@gmail.com>
2019-12-17 15:01:13 -0800

    Cleaned the solution now that I know more about Python (and programming)

--- 02-linked_lists/2.1-remove_dups.py
+++ 02-linked_lists/2.1-remove_dups.py
@@ -1,41 +1,49 @@
 from linked_list import *
 
+# O(n) runtime, O(n) space
 def remove_dups(head):
-	nodes = {head.data}
-	n = head
-	while n.next is not None:
-		if n.next.data in nodes:
-			n.next = n.next.next
-		else:
-			nodes.add(n.next.data)
-			n = n.next
+   nodes = set()
+   prev, n = None, head
+   while n is not None:
+      if n.data in nodes:
+         prev.next = n.next
+      else:
+         nodes.add(n.data)
+         prev = n 
+      n = n.next
 
+
+# O(n^2) runtime, O(1) space - temporary buffer not allowed
 def remove_dups2(head):
 	if head is None or head.next is None:
 		return
+
 	n1 = head
-	while n1.next is not None:
-		n2 = n1
-		while n2.next is not None:
-			if n2.next.data == n1.data:
-				n2.next = n2.next.next
+	while n1 is not None:
+		prev, n2 = n1, n1.next
+		while n2 is not None:
+			if n2.data == n1.data:
+				prev.next = n2.next
 			else:
-				n2 = n2.next
+				prev = n2
+			n2 = n2.next
 		n1 = n1.next
+
 
 # Let's test it!
 if __name__ == '__main__':
-	head = Node(2)
-	head.append(2)
-	head.append(2)
-	head.append(3)
-	head.append(7)
-	head.append(2)
-	head.append(3)
-	head.append(5)
-	head.append(1)
-	head.append(2)
+	foo = Node(2)
+	foo.append(2)
+	foo.append(2)
+	foo.append(3)
+	foo.append(7)
+	foo.append(2)
+	foo.append(3)
+	foo.append(5)
+	foo.append(1)
+	foo.append(2)
 
-	print_l(head)
-	remove_dups2(head)
-	print_l(head)
+	print_l(foo)
+	remove_dups(foo)
+	#remove_dups2(foo)
+	print_l(foo)
