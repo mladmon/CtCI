7c764451b10a8303136acdb35d8745cf2abc41df
Mario Admon <mladmon589@gmail.com>
2019-12-16 16:44:08 -0800

    Cleaned the solution now that I know more about Python

--- 01-arrays_strings/1.6-str_compress.py
+++ 01-arrays_strings/1.6-str_compress.py
@@ -1,57 +1,36 @@
-def str_compress(string):
+# O(n) runtime, O(n) space 
+def compress_str(string):
 	if len(string) < 3:
 		return string
-	comp, char, count = [], string[0], 1
-	for i in range(1, len(string)):
-		if string[i] is not char:
-			comp.append((char, count))
-			char, count = string[i], 1
-		else:
-			count += 1
-	comp.append((char, count)) # add the last (single or repeated) char
-	if len(comp)*2 < len(string):
-		return ''.join([c + str(i) for c, i in comp])
+
+	compressed_str = []
+	i = 0
+	while i < len(string):
+		compressed_str.append(string[i])
+		j = i + 1
+		while j < len(string) and string[i] == string[j]:
+			j += 1
+
+		seq_length = j - i
+		compressed_str.append(str(seq_length))
+		i = j
+
+	if len(compressed_str) < len(string):
+		return ''.join(compressed_str)
 	else:
 		return string
 
-def str_compress2(string):
-	if len(string) < 3:
-		return string
-	comp, count = [], 0
-	for i in range(len(string)):
-		count += 1
-		if i + 1 is len(string) or string[i+1] is not string[i]:
-			comp.extend((string[i], str(count)))
-			count = 0
-	if len(comp) < len(string):
-		return ''.join(comp)
-	else:
-		return string
-
-# Let's test it!
-print('aabcccccaaa', str_compress('aabcccccaaa'))
-print(repr(''), str_compress(''))
-print('a', str_compress('a'))	
-print('aa', str_compress('aa'))
-print('ab', str_compress('ab'))
-print('aaa', str_compress('aaa'))
-print('aabb', str_compress('aabb'))
-print('aaab', str_compress('aaab'))
-print('aaabb', str_compress('aaabb'))
-print('Ooooooh!!', str_compress('Ooooooh!!'))
-print('oooOoOOO', str_compress('oooOoOOO'))
-print('oooOOoOOO', str_compress('oooOOoOOO'))
 
 print()
-print('aabcccccaaa', str_compress2('aabcccccaaa'))
-print(repr(''), str_compress2(''))
-print('a', str_compress2('a'))	
-print('aa', str_compress2('aa'))
-print('ab', str_compress2('ab'))
-print('aaa', str_compress2('aaa'))
-print('aabb', str_compress2('aabb'))
-print('aaab', str_compress2('aaab'))
-print('aaabb', str_compress2('aaabb'))
-print('Ooooooh!!', str_compress2('Ooooooh!!'))
-print('oooOoOOO', str_compress2('oooOoOOO'))
-print('oooOOoOOO', str_compress2('oooOOoOOO'))
+print('aabcccccaaa', compress_str('aabcccccaaa'))
+print(repr(''), compress_str(''))
+print('a', compress_str('a'))	
+print('aa', compress_str('aa'))
+print('ab', compress_str('ab'))
+print('aaa', compress_str('aaa'))
+print('aabb', compress_str('aabb'))
+print('aaab', compress_str('aaab'))
+print('aaabb', compress_str('aaabb'))
+print('Ooooooh!!', compress_str('Ooooooh!!'))
+print('oooOoOOO', compress_str('oooOoOOO'))
+print('oooOOoOOO', compress_str('oooOOoOOO'))
