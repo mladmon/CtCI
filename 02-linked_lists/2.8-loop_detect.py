40e65d841775230099b6b12fa63d62c6cdc84ef4
Mario Admon <mladmon589@gmail.com>
2019-12-29 11:58:29 -0500

    Cleaned the solution now that I know more about Python (and programming)

--- 02-linked_lists/2.8-loop_detect.py
+++ 02-linked_lists/2.8-loop_detect.py
@@ -1,75 +1,43 @@
 from linked_list import *
 
+# O(n) runtime, O(n) space - use a set to check for duplicate node
 def loop_detect(head):
-	n, nodes = head, set()
-	while n is not None:
-		if n in nodes:
-			return n
-		else:
-			nodes.add(n)
-			n = n.next
-	return None
+	n = head
+	nodes = set()
+	while n not in nodes:
+		nodes.add(n)
+		n = n.next
 
+	return n
+
+
+# O(n) runtime, O(1) space - use two pointers and difficult logic
 def loop_detect2(head):
-	n1, n2 = head, head
-	while n2 is not None and n2.next is not None:
-		n2 = n2.next.next
-		n1 = n1.next
-		if n1 is n2:
-			break
-	if n2 is None or n2.next is None:
-		return None
-	# loop exists and n1, n2 (and head) are k steps away from loop beginning
-	n1 = head
-	while n1 is not n2:
-		n1, n2 = n1.next, n2.next
-	return n1
+	slow, fast = head.next, head.next.next
+	while slow is not fast:
+		slow = slow.next 
+		fast = fast.next.next
+
+	# slow, fast, and head are all k steps away from loop beginning
+	slow = head
+	while slow is not fast:
+		slow = slow.next
+		fast = fast.next
+
+	return slow
+
 
 # Let's test it!
-l1 = Node('A')
-l1.append('B')
-l1.append('C')
-l1.append('D')
-l1.append('E')
-l1.next.next.next.next.next = l1.next.next
-print('A B C D E C ...')
-n = loop_detect2(l1)
-print('loop_detect:', n.data, n)
+foo = Node(1)
+foo.append(2)
+foo.append(3)
+foo.append(4)
+foo.append(5)
 
-l2 = Node(5)
-l2.append(1)
-l2.append(3)
-l2.append(7)
-print_list(l2)
-print('loop_detect:', loop_detect2(l2))
+five = foo.search(5)
+three = foo.search(3)
+five.next = three
 
-l3 = Node(6)
-l3.append(5)
-l3.append(1)
-l3.append(3)
-l3.append(7)
-
-print_list(l3)
-print('loop_detect:', loop_detect2(l3))
-
-l4 = Node(5)
-l4.next = l4
-print('5 5 ...')
-n = loop_detect2(l4)
-print('loop_detect:', n.data, n)
-
-l5 = Node(5)
-print_list(l5)
-print('loop_detect:', loop_detect2(l5))
-
-l6 = Node(30)
-l6.append(35)
-l6.next.next = l6
-print('30 35 30 ...')
-n = loop_detect2(l6)
-print('loop_detect:', n.data, n)
-
-l7 = Node(30)
-l7.append(35)
-print_list(l7)
-print('loop_detect:', loop_detect2(l7))
+print('foo: 1 2 3 4 5 3 4 5 ...')
+loop_start = loop_detect2(foo)
+print('loop detected at node:', loop_start.data, f'({loop_start})')
