3521cd0715bdc1bf931843d25121be7ad6c3b9b5
Mario Admon <mladmon589@gmail.com>
2020-01-04 18:23:54 -0500

    Cleaned the solution now that I know more about Python (and programming)

--- 04-trees_graphs/4.4-check_balanced.py
+++ 04-trees_graphs/4.4-check_balanced.py
@@ -4,38 +4,49 @@ class Node:
 		self.left = None
 		self.right = None
 
-def check_balanced(n):
-	if n is None:
-		return True
-	h, balanced = check_helper(n)
+
+# O(n) runtime, O(n) space - depth of recursion is O(n) if not balanced
+def check_balanced(root):
+	height, balanced = check_helper(root)
 	return balanced
+
 
 def check_helper(n):
 	if n is None:
-		return (0, True)
-	h_left, balanced = check_helper(n.left)
-	if balanced:
-		h_right, balanced = check_helper(n.right)
-		print(n.key, h_left, h_right)
-		if balanced:
-			diff = h_left - h_right
-			if diff < -1 or diff > 1:
-				return (-1, False)
-			else:
-				largest = h_left if h_left > h_right else h_right
-				return (largest + 1, True)
-	return (-1, False) # don't care about height
+		return (-1, True)
+
+	left_height, balanced = check_helper(n.left)
+	if not balanced:
+		return (0, False)
+
+	right_height, balanced = check_helper(n.right)
+	if not balanced:
+		return (0, False)
+
+	print(n.key, left_height, right_height, sep='\t')
+	diff = abs(left_height - right_height)
+	if diff > 1:
+		return (0, False)
+		
+	max_height = left_height if left_height > right_height else right_height
+	return (max_height + 1, True)
+
 
 # Let's test it!
-r = Node(1)
-r.left, r.right = Node(2), Node(3)
-r.left.left, r.left.right = Node(4), Node(5)
-r.right.left, r.right.right = Node(6), Node(7)
-r.left.left.left = Node(8)
-print(check_balanced(r))
+foo = Node(1)
+foo.left, foo.right = Node(2), Node(3)
+foo.left.left, foo.left.right = Node(4), Node(5)
+foo.right.left, foo.right.right = Node(6), Node(7)
+foo.left.left.left = Node(8)
+print('node\tleft\tright')
+print(check_balanced(foo))
+print()
 
-r.left.left.right = Node(9)
-print(check_balanced(r))
+foo.left.left.right = Node(9)
+print('node\tleft\tright')
+print(check_balanced(foo))
+print()
 
-r.left.left.right.left = Node(10)
-print(check_balanced(r))
+foo.left.left.right.left = Node(10)
+print('node\tleft\tright')
+print(check_balanced(foo))
